5cc3b99feef78b6d40552358779f13f5d053f953
EG <erick.g.amaris@gail.com>
2022-10-06 17:38:43 -0500

    Error

--- proyectoPrueba/pagina/urls.py
+++ proyectoPrueba/pagina/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
-
+app_name="pagina"
 urlpatterns = [
 	#localhost:8000/pagina
 	path("", views.index, name="index"),
 	#localhost:8000/pagina/peliculas
-	path("peliculas/", views.peliculas, name="peliculas")
+	path("todosLosUsuarios", views.todosLosUsuarios, name="todosLosUsuarios"),
+	path("registrarse", views.vistaRegistroUsuario, name="registrarse"),
+	path('registrarUsuario', views.registrarUsuario, name="registrarUsuario")
 ]
--- proyectoPrueba/pagina/views.py
+++ proyectoPrueba/pagina/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import usuarios
+from django.utils import timezone
+from django.urls import reverse
 
 def index( request ):
-	return HttpResponse("""
-		<h1>Hola Tripulantes</h1>
+	_usuarios = usuarios.objects.all()
+
+	return render( request, 'pagina/index.html')
+
+def todosLosUsuarios( request ):
+	_usuarios = usuarios.objects.all()
+
+	return render( request, 'pagina/todosLosUsuarios.html',{
+			'usuarios': _usuarios
+		})
+
+def registrarUsuario( request ):
+
+	try:
+
+		nombre = request.POST['nombre']
+		contrasenia = request.POST['nombre']
+		edad = request.POST['nombre']
+		dia_registro = timezone.now()
+	except:
+		return HttpResponse("Error")
+	else:
+		nuevoUsuario = usuarios( 
+			nombre=nombre, 
+			edad=edad,
+			dia_registro=dia_registro,
+			contrasenia=contrasenia)
+		nuevoUsuario.save()
+		return 	HttpResponseRedirect( reverse( "pagina:index" ) )	
 
 
-
-		""")
-
-
-def peliculas( request ):
-	return HttpResponse("""
-		<h1 style="text-aling: center">Peliculas<h1>
-
-
-		""")
+def vistaRegistroUsuario( request ):
+	return render( request, 'pagina/registrarse.html')
